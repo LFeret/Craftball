2caf723dc47b419fa43194f85901166d8a4e5326
mandybuchhold <mandybuchhold@googlemail.com>
2020-08-30 17:58:58 +0200

    Collison vom Player gefixt

--- addons/godot-openvr/scenes/left_controller.gd
+++ addons/godot-openvr/scenes/left_controller.gd
@@ -11,6 +11,9 @@ export var smooth_rotation = false
 export var smooth_turn_speed = 2.0
 export var step_turn_delay = 0.2
 export var step_turn_angle = 20.0
+export var drag_factor = 0.1
+export var player_radius = 0.4
+export var max_speed = 5.0
 
 var turn_step = 0.0
 var origin_node = null
@@ -34,7 +37,7 @@ func _process(delta):
 		# you are using and which OS you are on.
 #	var trackpad_vector = Vector2(-get_joystick_axis(1), get_joystick_axis(0))
 	var joystick_vector = Vector2(-get_joystick_axis(5), get_joystick_axis(4))
-	
+
 #	if trackpad_vector.length() < CONTROLLER_DEADZONE:
 #		trackpad_vector = Vector2(0, 0)
 #	else:
@@ -47,16 +50,16 @@ func _process(delta):
 
 	var forward_direction = get_parent().get_node("ARVRCamera").global_transform.basis.z.normalized()
 	var right_direction = get_parent().get_node("ARVRCamera").global_transform.basis.x.normalized()
-	
+
 	#var movement_vector = (trackpad_vector + joystick_vector).normalized()
 	var movement_vector = (joystick_vector).normalized()
-	
+
 	var movement_forward = forward_direction * movement_vector.x * delta * MOVEMENT_SPEED
 	var movement_right = right_direction * movement_vector.y * delta * MOVEMENT_SPEED
-	
+
 	movement_forward.y = 0
 	movement_right.y = 0
-	
+
 	if movement_right.length() > 0 or movement_forward.length() > 0:
 		get_parent().translate(movement_right + movement_forward)
 		directional_movement = true
@@ -71,12 +74,12 @@ func _process(delta):
 			var t1 = Transform()
 			var t2 = Transform()
 			var rot = Transform()
-			
+
 			t1.origin = -camera_node.transform.origin
 			t2.origin = camera_node.transform.origin
 			rot = rot.rotated(Vector3(0.0, -1.0, 0.0), smooth_turn_speed * delta * left_right)
 			origin_node.transform *= t2 * rot * t1
-			
+
 			# reset turn step, doesn't apply
 			turn_step = 0.0
 		else:
@@ -84,24 +87,24 @@ func _process(delta):
 				if turn_step < 0.0:
 					# reset step
 					turn_step = 0
-				
+
 				turn_step += left_right * delta
 			else:
 				if turn_step > 0.0:
 					# reset step
 					turn_step = 0
-				
+
 				turn_step += left_right * delta
-			
+
 			if abs(turn_step) > step_turn_delay:
 				# we rotate around our Camera, but we adjust our origin, so we need a little bit of trickery
 				var t1 = Transform()
 				var t2 = Transform()
 				var rot = Transform()
-				
+
 				t1.origin = -camera_node.transform.origin
 				t2.origin = camera_node.transform.origin
-				
+
 				# Rotating
 				while abs(turn_step) > step_turn_delay:
 					if (turn_step > 0.0):
@@ -110,8 +113,13 @@ func _process(delta):
 					else:
 						rot = rot.rotated(Vector3(0.0, -1.0, 0.0), step_turn_angle * PI / 180.0)
 						turn_step += step_turn_delay
-				
+
 				origin_node.transform *= t2 * rot * t1
+
 	else:
 		# reset turn step, no longer turning
 		turn_step = 0.0
+		
+		
+	################################################################
+	#	var playerBody = get_parent().get_node("player_body")
