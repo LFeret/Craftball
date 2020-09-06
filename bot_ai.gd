cdd4d8b9f1ccaf3dc811e2975c64b6d5f9f2012a
Leander Féret <l.feret@fachnotizen.de>
2020-09-06 11:47:13 +0200

    [WIP: Feature] Bot Ai Vorbereitet. UNGETESTET!!!

--- bot_ai.gd
+++ bot_ai.gd
@@ -5,8 +5,43 @@ extends Node
 var bots = []
 var game_start = false
 
+var time = 0
+var timerVar = 0
+
+var scan_intervall = 3
+var scan_radius = 5
+var scan = false
+
 func register_bot(bot):
 	bots.append(bot)
 
 func start():
 	game_start = true
+
+func _process(delta):
+	timerVar += delta
+	
+	if timerVar >= 1:
+		# ~ 1 Second gone
+		time += 1
+		timerVar = 0
+	
+	if time % scan_intervall == 0:
+		time = 0
+		scan = true
+	
+	for bot in bots:
+		if scan:
+			var networking = get_node("/root/global/").networking
+			var player = null
+			for peer_id in networking.players:
+				player = networking.players[peer_id]
+			
+			var player_position = player.get_global_transform().origin
+			var bot_position = player.get_global_transform().origin
+			
+			var distance = bot_position.distance_to(player_position)
+			if distance <= scan_radius:
+				bot.set_player_pos(player_position)
+
+	scan = false
--- leander/bots/bot.gd
+++ leander/bots/bot.gd
@@ -1,15 +1,60 @@
-extends Spatial
+extends RigidBody
 
 
 var id
 var color
 var life = 5
+var current_ball
+var ball_type = 'normal_ball'
+
+const ball = preload("res://leander/ball/ball.res")
+const speed_ball = preload("res://leander/ball/speed_ball.res")
+const block_ball = preload("res://leander/ball/block_ball.res")
+
+var past_position = null
+var current_position = null
+
+var walking_direction = Vector3(1,0,1)
+var walking_speed = 100
+
+var throw_direction = Vector3(0,0,0)
+
+var throw_intervall = 5
+
+var time = 0
+var timerVar = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _process(delta):
+	timerVar += delta
+	
+	if timerVar >= 1:
+		# ~ 1 Second gone
+		time += 1
+		timerVar = 0
+		
+		if time % throw_intervall == 0:
+			create_ball()
+			throw_ball()
+	
+	# position logging - for throwing // leander stuff
+	if past_position == null:
+		past_position = global_transform # controler right now
+	elif current_position == null:
+		current_position = global_transform # controler right now
+	else:
+		past_position = current_position
+		current_position = global_transform # controler right now
 
+	set_linear_velocity(walking_direction * walking_speed * delta)
+
+func set_player_pos(player_pos):
+	walking_direction = player_pos
+	throw_direction = player_pos
+	
 func set_color(set_color) -> void:
 	color = set_color
 	var material = SpatialMaterial.new()
@@ -29,8 +74,21 @@ func set_color(set_color) -> void:
 		'black':
 			material.albedo_color = Color(0,0,0)
 	
-	$bot_body.get_child(0).set_surface_material(0, material)
-	
+	get_child(0).set_surface_material(0, material)
+
+func set_walking_direction(direction):
+	match direction:
+		'left':
+			pass
+		'right':
+			pass
+		
+func set_walking_speed(speed):
+	self.walking_speed = speed
+
+func set_throw_direction(direction):
+	self.throw_direction = direction
+
 func hit():
 	life -= 1
 	if life <= 0:
@@ -39,3 +97,50 @@ func hit():
 func die():
 	self.queue_free()
 	print("Bot Died. Grats")
+
+func create_ball():
+	match ball_type:
+		'normal_ball':
+			current_ball = ball.instance()
+		'speed_ball':
+			current_ball = speed_ball.instance()
+		'block_ball':
+			current_ball = block_ball.instance()
+		
+	current_ball.color = color
+	
+	# maybe get node by player_id is necesseray
+	
+	current_ball.sleeping = true
+	# Set Ball Position
+	get_parent().add_child(current_ball) # add to World
+
+func throw_ball():
+	current_ball.sleeping = false
+	current_ball.thrown = true
+	current_ball.current_player = self
+	
+	var force = null
+	# errechne Richtungs und Kraft Vector
+	if throw_direction == Vector3(0,0,0):
+		force = get_linear_velocity()
+	else:
+		force = throw_direction
+	
+	var t = global_transform
+
+	global_transform = t
+	mode = RigidBody.MODE_RIGID
+	collision_mask = 1
+	collision_layer = 1
+	
+	# set our starting velocity
+	linear_velocity = force * 10
+	
+	#apply_impulse(Vector3(0.0, 0.0, 0.0), impulse)
+
+func get_linear_velocity():
+	if past_position != null and current_position != null:
+		return (current_position.origin - past_position.origin) / 0.0166 # 0.0166 --> framerate (time-delta)
+	else:
+		return Vector3(0,0,0)
